f54ef5a04e31c54fff3e48b3559dda29ef6a2714
Tayyab Shahzad <k250685@nu.edu.pk>
2025-09-15 21:07:23 +0500

    Create task10.c

---
+++ task10.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+    printf("Full Name: Tayyab Shahzad\n");
+    printf("FAST NU ID: 25K-0685\n");
+    printf("\tTabbed Line\n");
+    printf("\nNew Line\n");
+    return 0;
+}
