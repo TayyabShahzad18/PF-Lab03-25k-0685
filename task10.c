5df8118bba109d8707870e11037eb9a5817ef4df
Tayyab Shahzad <k250685@nu.edu.pk>
2025-09-15 21:07:42 +0500

    Delete task10.c

--- task10.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main() {
-    printf("Full Name: Tayyab Shahzad\n");
-    printf("FAST NU ID: 25K-0685\n");
-    printf("\tTabbed Line\n");
-    printf("\nNew Line\n");
-    return 0;
-}
